50bb87c8c5bc103992bd9daea1e9945e4440d856
sarvesh42 <sarveshkumar5513@gmail.com>
2023-01-28 14:51:03 +0530

    flask app running in local server successfully

---
+++ Flask/client.py
@@ -0,0 +1,17 @@
+import requests
+import numpy as np
+url = "http://127.0.0.1:5000/predict"
+data = {"sepal_length" : 10,
+        "sepal_width" : 0.1,
+        "petal_length" : 0,
+        "petal_width" : 10,
+        }
+
+# data = np.array(data)
+# print(data)
+
+response = requests.post(url, json = data)
+print(response)
+print(response.text)
+
+
